feb42a0c9f83aec57a9bf7108b64bf45d88777f9
Michael Brailsford <michael.brailsford@cerner.com>
2014-08-23 13:36:05 -0500

    Renaming and touching up the project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
-(defproject storm_tracer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject storm_tracer "0.0.1-SNAPSHOT"
+  :description "A distributed ray tracer for storm, written in clojure."
+  :url "https://github.com/brailsmt/storm_tracer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :all
+  :dependencies [[storm "0.9.0.1"]
+                 [org.clojure/clojure "1.5.1"]
+                 [org.testng/testng "6.8.5"]
+                 [org.easytesting/fest-assert-core "2.0M8"]
+                 [org.mockito/mockito-all "1.9.0"]
+                 [org.jmock/jmock "2.6.0"]]
+  :min-lein-version "2.0.0"
+  )
